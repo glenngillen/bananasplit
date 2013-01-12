5d0c2dd0c2f2c528511467c7c90a86266ffd3461
Glenn Gillen <me@glenngillen.com>
2013-01-11 21:49:20 -0800

    Rename references in tests. We're done when it's all green.

--- test/abingo_test.rb
+++ test/abingo_test.rb
@@ -1,63 +1,63 @@
 require_relative 'test_helper'
 
-class AbingoTest < Test::Unit::TestCase
+class BananaSplitTest < Test::Unit::TestCase
 
   setup do
-    Abingo.options = {}
+    BananaSplit.options = {}
   end
 
   teardown do
-    Abingo.cache.clear
-    Abingo::Experiment.delete_all
-    Abingo::Alternative.delete_all
+    BananaSplit.cache.clear
+    BananaSplit::Experiment.delete_all
+    BananaSplit::Alternative.delete_all
   end
 
   test "identity automatically assigned" do
-    abingo = Abingo.identify
-    assert abingo.identity != nil
+    split = BananaSplit.identify
+    assert split.identity != nil
   end
 
   test "alternative parsing" do
     array = %w{a b c}
-    assert_equal array, Abingo.parse_alternatives(array)
-    assert_equal 65, Abingo.parse_alternatives(65).size
-    assert_equal 4, Abingo.parse_alternatives(2..5).size
-    assert !(Abingo.parse_alternatives(2..5).include? 1)
+    assert_equal array, BananaSplit.parse_alternatives(array)
+    assert_equal 65, BananaSplit.parse_alternatives(65).size
+    assert_equal 4, BananaSplit.parse_alternatives(2..5).size
+    assert !(BananaSplit.parse_alternatives(2..5).include? 1)
   end
 
   test "experiment creation" do
-    assert_equal 0, Abingo::Experiment.count
-    assert_equal 0, Abingo::Alternative.count
+    assert_equal 0, BananaSplit::Experiment.count
+    assert_equal 0, BananaSplit::Alternative.count
     alternatives = %w{A B}
-    abingo = Abingo.identify
-    alternative_selected = abingo.test("unit_test_sample_A", alternatives)
-    assert_equal 1, Abingo::Experiment.count
-    assert_equal 2, Abingo::Alternative.count
+    split = BananaSplit.identify
+    alternative_selected = split.test("unit_test_sample_A", alternatives)
+    assert_equal 1, BananaSplit::Experiment.count
+    assert_equal 2, BananaSplit::Alternative.count
     assert alternatives.include?(alternative_selected)
   end
 
   test "exists works right" do
-    abingo = Abingo.identify
-    abingo.test("exist works right", %w{does does_not})
-    assert Abingo::Experiment.exists?("exist works right")
+    split = BananaSplit.identify
+    split.test("exist works right", %w{does does_not})
+    assert BananaSplit::Experiment.exists?("exist works right")
   end
 
   test "alternatives picked consistently" do
-    abingo = Abingo.identify
-    alternative_picked = abingo.test("consistency_test", 1..100)
+    split = BananaSplit.identify
+    alternative_picked = split.test("consistency_test", 1..100)
     100.times do
-      assert_equal alternative_picked, abingo.test("consistency_test", 1..100)
+      assert_equal alternative_picked, split.test("consistency_test", 1..100)
     end
   end
 
   test "participation works" do
     new_tests = %w{participationA participationB participationC}
-    abingo = Abingo.identify
+    split = BananaSplit.identify
     new_tests.map do |test_name|
-      abingo.test(test_name, 1..5)
+      split.test(test_name, 1..5)
     end
 
-    participating_tests = Abingo.cache.read("Abingo::participating_tests::#{abingo.identity}") || []
+    participating_tests = BananaSplit.cache.read("BananaSplit::participating_tests::#{split.identity}") || []
 
     new_tests.map do |test_name|
       assert participating_tests.include? test_name
@@ -66,53 +66,53 @@ class AbingoTest < Test::Unit::TestCase
 
   test "participants counted" do
     test_name = "participants_counted_test"
-    abingo = Abingo.identify
-    alternative = abingo.test(test_name, %w{a b c})
+    split = BananaSplit.identify
+    alternative = split.test(test_name, %w{a b c})
 
-    ex = Abingo::Experiment.find_by_test_name(test_name)
-    lookup = Abingo::Alternative.calculate_lookup(test_name, alternative)
-    chosen_alt = Abingo::Alternative.find_by_lookup(lookup)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
+    lookup = BananaSplit::Alternative.calculate_lookup(test_name, alternative)
+    chosen_alt = BananaSplit::Alternative.find_by_lookup(lookup)
     assert_equal 1, ex.participants
     assert_equal 1, chosen_alt.participants
   end
 
   test "conversion tracking by test name" do
     test_name = "conversion_test_by_name"
-    abingo = Abingo.identify
-    alternative = abingo.test(test_name, %w{a b c})
-    abingo.bingo!(test_name)
-    ex = Abingo::Experiment.find_by_test_name(test_name)
-    lookup = Abingo::Alternative.calculate_lookup(test_name, alternative)
-    chosen_alt = Abingo::Alternative.find_by_lookup(lookup)
+    split = BananaSplit.identify
+    alternative = split.test(test_name, %w{a b c})
+    split.bingo!(test_name)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
+    lookup = BananaSplit::Alternative.calculate_lookup(test_name, alternative)
+    chosen_alt = BananaSplit::Alternative.find_by_lookup(lookup)
     assert_equal 1, ex.conversions
     assert_equal 1, chosen_alt.conversions
-    abingo.bingo!(test_name)
+    split.bingo!(test_name)
 
     #Should still only have one because this conversion should not be double counted.
     #We haven't specified that in the test options.
-    assert_equal 1, Abingo::Experiment.find_by_test_name(test_name).conversions
+    assert_equal 1, BananaSplit::Experiment.find_by_test_name(test_name).conversions
   end
 
   test "conversion tracking by conversion name" do
-    abingo = Abingo.identify
+    split = BananaSplit.identify
     conversion_name = "purchase"
     tests = %w{conversionTrackingByConversionNameA conversionTrackingByConversionNameB conversionTrackingByConversionNameC}
     tests.map do |test_name|
-      abingo.test(test_name, %w{A B}, :conversion => conversion_name)
+      split.test(test_name, %w{A B}, :conversion => conversion_name)
     end
 
-    abingo.bingo!(conversion_name)
+    split.bingo!(conversion_name)
     tests.map do |test_name|
-      assert_equal 1, Abingo::Experiment.find_by_test_name(test_name).conversions
+      assert_equal 1, BananaSplit::Experiment.find_by_test_name(test_name).conversions
     end
   end
 
   test "short circuiting works" do
     conversion_name = "purchase"
     test_name = "short circuit test"
-    abingo = Abingo.identify
-    alt_picked = abingo.test(test_name, %w{A B}, :conversion => conversion_name)
-    ex = Abingo::Experiment.find_by_test_name(test_name)
+    split = BananaSplit.identify
+    alt_picked = split.test(test_name, %w{A B}, :conversion => conversion_name)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
     alt_not_picked = (%w{A B} - [alt_picked]).first
 
     ex.end_experiment!(alt_not_picked, conversion_name)
@@ -120,10 +120,10 @@ class AbingoTest < Test::Unit::TestCase
     ex.reload
     assert_equal "Finished", ex.status
 
-    abingo.bingo!(test_name)  #Should not be counted, test is over.
+    split.bingo!(test_name)  #Should not be counted, test is over.
     assert_equal 0, ex.conversions
 
-    new_bingo = Abingo.identify("shortCircuitTestNewIdentity")
+    new_bingo = BananaSplit.identify("shortCircuitTestNewIdentity")
     new_bingo.test(test_name, %w{A B}, :conversion => conversion_name)
     ex.reload
     assert_equal 1, ex.participants  #Original identity counted, new identity not counted b/c test stopped
@@ -137,13 +137,13 @@ class AbingoTest < Test::Unit::TestCase
     threads = []
     5.times do
       threads << Thread.new do
-        abingo = Abingo.identify
-        abingo.test(test_name, alternatives, :conversion => conversion_name)
+        split = BananaSplit.identify
+        split.test(test_name, alternatives, :conversion => conversion_name)
         ActiveRecord::Base.connection.close
       end
     end
     threads.each(&:join)
-    assert_equal 1, Abingo::Experiment.count_by_sql(["select count(id) from experiments where test_name = ?", test_name])
+    assert_equal 1, BananaSplit::Experiment.count_by_sql(["select count(id) from experiments where test_name = ?", test_name])
   end
 
   test "proper conversions with concurrency" do
@@ -152,31 +152,31 @@ class AbingoTest < Test::Unit::TestCase
     threads = []
     alternatives.size.times do |i|
       threads << Thread.new do
-        abingo = Abingo.identify(i)
-        abingo.test(test_name, alternatives)
-        abingo.bingo!(test_name)
+        split = BananaSplit.identify(i)
+        split.test(test_name, alternatives)
+        split.bingo!(test_name)
         sleep(0.3) if i == 0
         ActiveRecord::Base.connection.close
       end
     end
     threads.each(&:join)
-    ex = Abingo::Experiment.find_by_test_name(test_name)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
     ex.alternatives.each do |alternative|
       assert_equal 1, alternative.conversions
     end
   end
 
   test "non-humans are ignored for participation and conversions if not explicitly counted" do
-    Abingo.options[:count_humans_only] = true
-    Abingo.options[:expires_in] = 1.hour
-    Abingo.options[:expires_in_for_bots] = 3.seconds
-    first_identity = Abingo.identify("unsure_if_human#{Time.now.to_i}")
+    BananaSplit.options[:count_humans_only] = true
+    BananaSplit.options[:expires_in] = 1.hour
+    BananaSplit.options[:expires_in_for_bots] = 3.seconds
+    first_identity = BananaSplit.identify("unsure_if_human#{Time.now.to_i}")
     test_name = "are_you_a_human"
     first_identity.test(test_name, %w{does_not matter})
 
     assert !first_identity.is_human?, "Identity not marked as human yet."
 
-    ex = Abingo::Experiment.find_by_test_name(test_name)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
     first_identity.bingo!(test_name)
     assert_equal 0, ex.participants, "Not human yet, so should have no participants."
     assert_equal 0, ex.conversions, "Not human yet, so should have no conversions."
@@ -184,36 +184,36 @@ class AbingoTest < Test::Unit::TestCase
     first_identity.human!
 
     #Setting up second participant who doesn't convert.
-    second_identity = Abingo.identify("unsure_if_human_2_#{Time.now.to_i}")
+    second_identity = BananaSplit.identify("unsure_if_human_2_#{Time.now.to_i}")
     second_identity.test(test_name, %w{does_not matter})
     second_identity.human!
 
-    ex = Abingo::Experiment.find_by_test_name(test_name)
+    ex = BananaSplit::Experiment.find_by_test_name(test_name)
     assert_equal 2, ex.participants, "Now that we're human, our participation should matter."
     assert_equal 1, ex.conversions, "Now that we're human, our conversions should matter, but only one of us converted."
   end
 
   test "Participating tests for a given identity" do
-    abingo = Abingo.identify("test_participant")
+    split = BananaSplit.identify("test_participant")
     test_names = (1..3).map {|t| "participating_test_test_name #{t}"}
     test_alternatives = %w{yes no}
-    test_names.each {|test_name| abingo.test(test_name, test_alternatives)}
-    ex = Abingo::Experiment.last
+    test_names.each {|test_name| split.test(test_name, test_alternatives)}
+    ex = BananaSplit::Experiment.last
     ex.end_experiment!("no")  #End final of 3 tests, leaving 2 presently running
 
-    assert_equal 2, abingo.participating_tests.size  #Pairs for two tests
-    abingo.participating_tests.each do |key, value|
+    assert_equal 2, split.participating_tests.size  #Pairs for two tests
+    split.participating_tests.each do |key, value|
       assert test_names.include? key
       assert test_alternatives.include? value
     end
 
-    assert_equal 3, abingo.participating_tests(false).size #pairs for three tests
-    abingo.participating_tests(false).each do |key, value|
+    assert_equal 3, split.participating_tests(false).size #pairs for three tests
+    split.participating_tests(false).each do |key, value|
       assert test_names.include? key
       assert test_alternatives.include? value
     end
 
-    non_participant = Abingo.identify("test_nonparticipant")
+    non_participant = BananaSplit.identify("test_nonparticipant")
     assert_equal({}, non_participant.participating_tests)
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,12 @@
-#require 'rubygems'
-#require 'rails'
-#
-#require 'rails/all'
 require 'test/unit'
 require 'contest'
 require 'pg'
 require 'active_support/cache'
 require 'active_support/cache/memory_store'
-require_relative '../lib/abingo'
+require_relative '../lib/bananasplit'
 ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
-  :database => "abingo-test")
+  :database => "bananasplit-test")
 ActiveRecord::Schema.define do
   create_table "experiments", :force => true do |t|
     t.string "test_name"
@@ -33,27 +29,4 @@ ActiveRecord::Schema.define do
   add_index "alternatives", "experiment_id"
   add_index "alternatives", "lookup"  #Critical for speed, since we'll primarily be updating by that.
 end
-Abingo.cache = ActiveSupport::Cache::MemoryStore.new
-#
-#require 'active_support'
-#require 'active_support/railtie'
-#require 'active_support/core_ext'
-#require 'active_support/test_case'
-#
-#require 'action_controller'
-#require 'action_controller/caching'
-#require 'active_record'
-#require 'active_record/base'
-#
-#require 'rails'
-#require 'rails/application'
-#
-#require 'rails/railtie'
-#
-##We need to load the whole Rails application to properly initialize Rails.cache and other constants.  Oh boy.
-##We're going to parse it out of RAILS_PATH/config.ru using a little metaprogramming magic.
-#require ::File.expand_path('../../../../../config/environment',  __FILE__)
-#lines = File.open(::File.expand_path('../../../../../config.ru',  __FILE__)).readlines.select {|a| a =~ /::Application/}
-#application_name = lines.first[/[^ ]*::/].gsub(":", "")
-#Kernel.const_get(application_name).const_get("Application").initialize!
-#
+BananaSplit.cache = ActiveSupport::Cache::MemoryStore.new
